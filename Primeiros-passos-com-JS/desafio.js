a5d4fc5d5b463d79bb7eae56270a4a811bbac60b
KaueSiqueira <kaue54150@gmail.com>
2024-11-20 17:12:53 -0300

    to-fixed

--- Primeiros-passos-com-JS/desafio.js
+++ Primeiros-passos-com-JS/desafio.js
@@ -1,10 +1,10 @@
 
 const precoCombustivel = 5.79;
-const gastoMedioCombustivelPorKm = 1;
+const gastoMedioCombustivelPorKm = 10;
 const distanciaKm = 1580;
 
 const calculo = precoCombustivel * (distanciaKm / gastoMedioCombustivelPorKm);
 
-console.log("O total gasto na viagem foi de R$: " + calculo);
+console.log("O total gasto na viagem foi de R$: " + calculo.toFixed(2));
 
 
