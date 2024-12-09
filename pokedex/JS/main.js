bafc50603be14220220f11f3151e52d758f19912
KaueSiqueira <kaue54150@gmail.com>
2024-12-09 15:25:29 -0300

    primeira-requisição

--- pokedex/JS/main.js
+++ pokedex/JS/main.js
@@ -1,1 +1,13 @@
-console.log("olá")
+
+
+
+const offSet = 0
+const limit = 10
+const url = "https://pokeapi.co/api/v2/pokemon?offset=${offSet}&limit=${limit}"
+
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+const x = 10 * 10
+console.log(x);
